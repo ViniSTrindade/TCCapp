af4ad63b2538aa6c13a56b061c1dcd980089a7b2
vinicius <viniciusnemtrindade@gmail.com>
2022-05-06 03:50:39 -0300

    definindo separação de fonemas por categoria

--- AFI10/app/src/main/java/com/vinicius/afi10/Adapter.java
+++ AFI10/app/src/main/java/com/vinicius/afi10/Adapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.graphics.Color;
 import android.icu.text.Transliterator;
 import android.media.MediaPlayer;
+import android.opengl.Visibility;
 import android.os.Handler;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -71,46 +73,125 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         int pos = position;
         TrackModel track = array.get(pos);
         holder.textView.setText(array.get(pos).getName());
-        holder.textView.setTextColor(Color.BLACK);
+//        holder.textView.setTextColor(Color.BLACK);
+
+        switch (pos) {
+            case 3: {
+                view.setVisibility(view.INVISIBLE);
+            }
+            case 9: {
+                view.setVisibility(view.INVISIBLE);
+            }
+            case 27: {
+                view.setVisibility(view.INVISIBLE);
+            }
+            case 33: {
+                view.setVisibility(view.INVISIBLE);
+            }
+            case 38: {
+                view.setVisibility(view.INVISIBLE);
+            }
+            case 39: {
+                view.setVisibility(view.INVISIBLE);
+            }
+        }
+
+        if (pos >= 13 && pos <= 16) {
+            view.setVisibility(view.INVISIBLE);
+        }
+        else if (pos >= 18 && pos <= 23) {
+            view.setVisibility(view.INVISIBLE);
+        }
+        else if (pos >= 42 && pos <= 44) {
+            view.setVisibility(view.INVISIBLE);
+        }
+        else if (pos >= 50 && pos <= 53) {
+            view.setVisibility(view.INVISIBLE);
+        }
+        else { }
+
+        if (pos >=0 && pos <=2){
+            holder.textView.setBackgroundColor(Color.YELLOW);
+        }
+        else if (pos >=6 && pos <=8){
+            holder.textView.setBackgroundColor(Color.YELLOW);
+        }
+        else if (pos == 12){
+            holder.textView.setBackgroundColor(Color.YELLOW);
+        }
+        else if (pos >=4 && pos <=5){
+            holder.textView.setBackgroundColor(Color.GREEN);
+        }
+        else if (pos >=10 && pos <=11){
+            holder.textView.setBackgroundColor(Color.GREEN);
+        }
+        else if (pos ==17){
+            holder.textView.setBackgroundColor(Color.GREEN);
+        }
+        else if (pos >=24 && pos <=26){
+            holder.textView.setBackgroundColor(Color.LTGRAY);
+        }
+        else if (pos >=30 && pos <=32){
+            holder.textView.setBackgroundColor(Color.LTGRAY);
+        }
+        else if (pos >=36 && pos <=37){
+            holder.textView.setBackgroundColor(Color.LTGRAY);
+        }
+        else if (pos >=28 && pos <=29){
+            holder.textView.setBackgroundColor(Color.BLUE);
+        }
+        else if (pos >=34 && pos <=35){
+            holder.textView.setBackgroundColor(Color.BLUE);
+        }
+        else if (pos >=40 && pos <=41){
+            holder.textView.setBackgroundColor(Color.BLUE);
+        }
+        else if (pos >=45 && pos <=47){
+            holder.textView.setBackgroundColor(Color.BLUE);
+        }
+        else if (pos >=48 && pos <=49){
+            holder.textView.setBackgroundColor(Color.RED);
+        }
+        else if (pos >=54 && pos <=66){
+            holder.textView.setBackgroundColor(Color.RED);
+        }
+        else{}
 
         holder.textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                holder.textView.setTextColor(Color.BLACK);
-                view.isFocusableInTouchMode();
-                if (mediaPlayer !=null) {
-                    if (mediaPlayer.isPlaying()){
-                        mediaPlayer.stop();
-                        mediaPlayer.reset();
-                        track.setPlaying(false);
-                    }
-                }
-                try {
-                    mediaPlayer = MediaPlayer.create(context, track.getId());
-                    if (mediaPlayer.isPlaying()){
-                        mediaPlayer.stop();
-                        mediaPlayer.reset();
-                        track.setPlaying(false);
-                    }
-                    else {
-                        mediaPlayer.start();
-                        DURACAO = mediaPlayer.getDuration();
-
-                        view.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                view.setEnabled(true);
+                    @Override
+                    public void onClick(View view) {
+//                        holder.textView.setTextColor(Color.BLACK);
+                        if (mediaPlayer != null) {
+                            if (mediaPlayer.isPlaying()) {
+                                mediaPlayer.stop();
+                                mediaPlayer.reset();
+                                track.setPlaying(false);
                             }
-                        }, DURACAO);
-                        view.setEnabled(false);
-                        holder.textView.setTextColor(Color.BLACK);
+                        }
+                        try {
+                            mediaPlayer = MediaPlayer.create(context, track.getId());
+                            if (mediaPlayer.isPlaying()) {
+                                mediaPlayer.stop();
+                                mediaPlayer.reset();
+                                track.setPlaying(false);
+                            } else {
+                                mediaPlayer.start();
+                                DURACAO = mediaPlayer.getDuration();
+
+                                view.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        view.setEnabled(true);
+                                    }
+                                }, DURACAO);
+                                view.setEnabled(false);
+                                holder.textView.setTextColor(Color.BLACK);
+                            }
+                        } catch (Exception e) {
+                            Log.e("Exception", e.getMessage());
+                        }
                     }
-                }
-                catch (Exception e){
-                    Log.e("Exception", e.getMessage());
-                }
-            }
-        });
+                });
 
         holder.textView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
--- AFI10/app/src/main/java/com/vinicius/afi10/MainActivity.java
+++ AFI10/app/src/main/java/com/vinicius/afi10/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -51,56 +52,74 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadAfiObj(){
-
-//        Obj Vogais Curtas
-                array.add( new TrackModel(R.raw.audio_vc3, "ɪ", false));
-                array.add(new TrackModel(R.raw.audio_vc4, "e", false));
-                array.add(new TrackModel(R.raw.audio_vc5, "ʊ", false));
-                array.add(new TrackModel(R.raw.audio_vc6, "ɒ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ʌ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "æ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ə", false));
-//                Obj Vogais Longas
-                array.add(new TrackModel(R.raw.audio_vc7, "ɑː", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "iː", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "uː", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ɜː", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ɔː", false));
-//                Obj Ditongos
-                array.add(new TrackModel(R.raw.audio_vc7, "aɪ", false));
-               array.add(new TrackModel(R.raw.audio_vc7, "eɪ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "eə", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "əʊ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ɪə", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "aʊ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ɔɪ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ʊə", false));
-//                Obj Consoantes Sonoras
-                array.add(new TrackModel(R.raw.audio_vc7, "b", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "d", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "g", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "v", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "z", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ʒ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "dʒ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ð", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "m", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "n", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ŋ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "l", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "r", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "j", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "w", false));
-//                Obj Consoantes Mudas
-                array.add(new TrackModel(R.raw.audio_vc7, "p", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "t", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "k", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "f", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "s", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "ʃ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "tʃ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "θ", false));
-                array.add(new TrackModel(R.raw.audio_vc7, "h", false));
+//       Fonemas
+        array.add(new TrackModel(R.raw.audio_vc3, "e", false));
+        array.add(new TrackModel(R.raw.audio_vc4, "ɒ", false));
+        array.add(new TrackModel(R.raw.audio_vc5, "ə", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc6, "ɑː", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "iː", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ʊ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ʌ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "æ", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "uː", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ɜː", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ɪ", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ɔː", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "aɪ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "eɪ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "eə", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "p", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "t", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "əʊ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ɪə", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "aʊ", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "k", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "f", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ɔɪ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ʊə", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "s", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ʃ", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "tʃ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "θ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "h", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "b", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "d", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(0, "", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "g", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "v", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "z", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ʒ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "dʒ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ð", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "m", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "n", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "ŋ", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "l", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "r", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "j", false));
+        array.add(new TrackModel(R.raw.audio_vc7, "w", false));
     }
 
     private void estanciaAdapter() {
